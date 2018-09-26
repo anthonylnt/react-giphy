9b9e1bdc6aa2d69d22367f58841c7c898c53fc0e
Anthony Lionnet <lionnet.ant@gmail.com>
2018-09-26 13:28:05 +0200

    Adding style

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = props => <div>Hello, {props.name}</div>;
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false,
+      counter: 0
+    };
+  }
+
+  clickHandler = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter +1
+    });
+  }
+
+  render() {
+    return (
+      <div className={this.state.clicked ? "clicked" : null} onClick={this.clickHandler} role="button">
+        Hello, {this.props.name} {this.state.counter}
+      </div>
+    );
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
