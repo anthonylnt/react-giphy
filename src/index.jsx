b8f3cb32ccb42551ab41bbb63d989a2752aca041
Anthony Lionnet <lionnet.ant@gmail.com>
2018-09-26 13:58:14 +0200

    Adding search bar

--- src/index.jsx
+++ src/index.jsx
@@ -2,34 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: false,
-      counter: 0
-    };
-  }
-
-  clickHandler = () => {
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter +1
-    });
-  }
-
-  render() {
-    return (
-      <div className={this.state.clicked ? "clicked" : null} onClick={this.clickHandler} role="button">
-        Hello, {this.props.name} {this.state.counter}
-      </div>
-    );
-  }
-}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <App />,
+    root);
 }
